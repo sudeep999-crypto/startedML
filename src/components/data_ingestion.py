ff9654ed83e56d67643008bb8d816cecd744512d
sudeep999-crypto <sudeepdas851@gmail.com>
2026-06-06 19:53:38 +0530

    Hit the r2 score corresponding to the best model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,10 @@ from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
 
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 @dataclass
 class DataIngestionConfig:
         train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -48,5 +52,7 @@ if __name__ == "__main__":
         obj = DataIngestion()
         train_data , test_data = obj.initiate_data_ingestion()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data, test_data)
-                    
+        train_array, test_array,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+        model_trainer = ModelTrainer()
+        r2_score = model_trainer.initiate_model_trainer(train_array, test_array)
+        print(r2_score)
